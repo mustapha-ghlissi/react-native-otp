044796802ba16fbb4ce1705eacd7f0d636eb06ef
Mustapha GHLISSI <ghlissi.mustapha@gmail.com>
2024-07-18 17:33:34 +0000

    chore: release 1.3.0

--- lib/typescript/components/OTP.types.d.ts
+++ lib/typescript/components/OTP.types.d.ts
@@ -1,11 +1,13 @@
-import { TextStyle, StyleProp, ViewStyle } from 'react-native';
+import { TextStyle, ViewStyle } from 'react-native';
 export type OTPProps = {
     length?: number;
-    fillBorderColor?: string;
+    filledOtpColor?: string;
     enabledFillShadow?: boolean;
     cursorColor?: string;
     selectionColor?: string;
-    inputStyle?: TextStyle | StyleProp<ViewStyle>;
+    filledColor?: string;
+    outlineColor?: string;
+    inputStyle?: TextStyle | ViewStyle;
     onConfirm: (code: number) => void;
 };
 //# sourceMappingURL=OTP.types.d.ts.map
